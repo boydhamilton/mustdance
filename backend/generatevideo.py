312c26d37de547398e182b0c501945b70dcbbdfc
Evan Anderson <e.e.anderson@shaw.ca>
2025-11-16 04:31:35 -0800

    fix video generation

--- backend/generatevideo.py
+++ backend/generatevideo.py
@@ -1,5 +1,3 @@
-
-
 import librosa
 from moviepy import VideoFileClip, concatenate_videoclips
 from moviepy import video
@@ -17,5 +15,34 @@ def process_mp3tomp4(filename, id):
     print(f"got tempo {tempo}")
 
     # Convert beat frames to time
-    return librosa.frames_to_time(beat_frames, sr=sr)
+    beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 
+
+    four_bar_ref = 8 # (1/120 minutes per beat (we record 120 bpm) * 60) gives seconds per beat
+
+    four_bar_delta = beat_times[15] - beat_times[0]
+    print(four_bar_delta)
+    speedup_factor = four_bar_delta / four_bar_ref
+
+
+    # List of videos and their playback speeds
+    dance_moves = [
+        {"file": "resources/test.mp4"}
+    ]
+
+    clips = []
+
+    for v in dance_moves:
+        clip = VideoFileClip(v["file"])
+        
+        # Change playback speed
+        if speedup_factor != 1.0:
+            clip = video.fx.MultiplySpeed(speedup_factor).apply(clip)
+        
+        clips.append(clip)
+
+    # Concatenate all clips
+    final_clip = concatenate_videoclips(clips)
+
+    # Export final video
+    final_clip.write_videofile(f"output/{id}.mp4", codec="libx264", audio_codec="aac")
