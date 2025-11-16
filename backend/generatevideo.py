4d797c1461555c6bb9e6ce70f12870c408b98fd2
Boyd Hamilton <boydroberthamilton@gmail.com>
2025-11-15 21:33:58 -0800

    feat: generates video

---
+++ backend/generatevideo.py
@@ -0,0 +1,48 @@
+
+
+import librosa
+from moviepy import VideoFileClip, concatenate_videoclips
+from moviepy import video
+
+# Load the audio file
+y, sr = librosa.load("oneofthegirls.mp3")
+
+print("loaded")
+
+# Detect tempo and beat frames
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+
+print(f"got tempo {tempo}")
+
+# Convert beat frames to time
+beat_times = librosa.frames_to_time(beat_frames, sr=sr)
+
+
+four_bar_ref = 8 # (1/120 minutes per beat (we record 120 bpm) * 60) gives seconds per beat
+
+four_bar_delta = beat_times[15] - beat_times[0]
+print(four_bar_delta)
+speedup_factor = four_bar_delta / four_bar_ref
+
+
+# List of videos and their playback speeds
+dance_moves = [
+    {"file": "120met.mp4"}
+]
+
+clips = []
+
+for v in dance_moves:
+    clip = VideoFileClip(v["file"])
+    
+    # Change playback speed
+    if speedup_factor != 1.0:
+        clip = video.fx.MultiplySpeed(speedup_factor).apply(clip)
+    
+    clips.append(clip)
+
+# Concatenate all clips
+final_clip = concatenate_videoclips(clips)
+
+# Export final video
+final_clip.write_videofile("output.mp4", codec="libx264", audio_codec="aac")
